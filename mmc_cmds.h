023cc7cf2ad018f25d67accd8a6f73d05991de9d
Roman Peniaev <r.peniaev@gmail.com>
2014-08-12 23:25:45 +0900

    UPSTREAM: mmc-utils: RPMB: add support for 4 rpmb operations
    mmc rpmb write-key <rpmb device> <key file>
      Program authentication key which is 32 bytes length and stored in the specified file.
      Also you can specify '-' instead of key file path and utility will read the key from stdin.
      BEWARE: key can be programmed only once!
      Example:
        $ echo -n AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHH | mmc rpmb write-key /dev/mmcblk0rpmb -
    
    mmc rpmb read-counter <rpmb device>
      Counter value for the <rpmb device> will be read to stdout.
    
    mmc rpmb read-block <rpmb device> <address> <blocks count> <output file> [key file]
      Blocks of 256 bytes will be read from <rpmb device> to output file or stdout if '-'
      is specified instead of regular path. If key is specified - read data will be verified.
      Instead of regular path you can specify '-' and key will be read from stdin.
      Example:
        $ echo -n AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHH | \
            mmc rpmb read-block /dev/mmcblk0rpmb 0x02 2 /tmp/block -
      or read the block without verification
        $ mmc rpmb read-block /dev/mmcblk0rpmb 0x02 2 /tmp/block
    
    mmc rpmb write-block <rpmb device> <address> <256 byte data file> <key file>
      Block of 256 bytes will be written from data file to <rpmb device>.
      Also you can specify '-' instead of key file path or data file and utility will read the
      data from stdin.
      Example:
        $ (awk 'BEGIN {while (c++<256) printf "a"}' | echo -n AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHH) | \
          mmc rpmb write-block /dev/mmcblk0rpmb 0x02 - -
    
    Signed-off-by: Roman Pen <r.peniaev@gmail.com>
    Cc: Ulf Hansson <ulf.hansson@linaro.org>,
    Cc: Ben Gardiner <bengardiner@nanometrics.ca>,
    Signed-off-by: Chris Ball <chris@printf.net>
    (cherry picked from commit c6cb053ec59e7667e2140c320e2b7d5a90592a20)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    
    Conflicts:
    	mmc.c
    	mmc_cmds.c
    	mmc_cmds.h
    
    BUG=none
    TEST=Compiled, Installed on gnawty
    
    Change-Id: I4509aad4bd203646633c23e2afde36da75ec4852
    Reviewed-on: https://chromium-review.googlesource.com/337986
    Commit-Ready: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- mmc_cmds.h
+++ mmc_cmds.h
@@ -30,3 +30,7 @@ int do_status_get(int nargs, char **argv);
 int do_enh_area_set(int nargs, char **argv);
 int do_write_reliability_set(int nargs, char **argv);
 int do_emmc50_ffu(int nargs, char **argv);
+int do_rpmb_write_key(int nargs, char **argv);
+int do_rpmb_read_counter(int nargs, char **argv);
+int do_rpmb_read_block(int nargs, char **argv);
+int do_rpmb_write_block(int nargs, char **argv);
