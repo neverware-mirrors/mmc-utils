20379faa5b92b66c0db8259031a1cb7c5fe1d963
Tomas Melin <tomas.melin@vaisala.com>
2016-08-29 12:02:28 -0400

    UPSTREAM: Fix return value check from set_partitioning_setting_completed()
    Test was wrong way around. Function returns non-zero value on fail.
    
    BUG=None
    TEST=Compile
    
    Signed-off-by: Tomas Melin <tomas.melin@vaisala.com>
    Signed-off-by: Chris Ball <chris@printf.net>
    (cherry picked from commit e094e28d56ac5cb0b70a14e61c2a936837729eb8)
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    
    Change-Id: I844c9ab5483b3519b6e7b0336615b90ac0ba7feb
    Reviewed-on: https://chromium-review.googlesource.com/1114445
    Commit-Ready: Gwendal Grignou <gwendal@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- mmc_cmds.c
+++ mmc_cmds.c
@@ -1089,7 +1089,7 @@ int do_create_gp_partition(int nargs, char **argv)
 	if (ret)
 		exit(1);
 
-	if (!set_partitioning_setting_completed(dry_run, device, fd))
+	if (set_partitioning_setting_completed(dry_run, device, fd))
 		exit(1);
 
 	return 0;
@@ -1234,7 +1234,7 @@ int do_enh_area_set(int nargs, char **argv)
 
 	printf("Done setting ENH_USR area on %s\n", device);
 
-	if (!set_partitioning_setting_completed(dry_run, device, fd))
+	if (set_partitioning_setting_completed(dry_run, device, fd))
 		exit(1);
 
 	return 0;
@@ -1296,7 +1296,7 @@ int do_write_reliability_set(int nargs, char **argv)
 	printf("Done setting EXT_CSD_WR_REL_SET to 0x%02x on %s\n",
 		value, device);
 
-	if (!set_partitioning_setting_completed(dry_run, device, fd))
+	if (set_partitioning_setting_completed(dry_run, device, fd))
 		exit(1);
 
 	return 0;
